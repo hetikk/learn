25a7c5d8857353696419b66efa1b9fffd10e686c
hetikk <hkastuev99@inbox.ru>
2022-05-28 00:00:20 +0300

    [auth/jwt] add liquibase

---
+++ Makefile
@@ -0,0 +1,31 @@
+.PHONY: jar-build tests-run env-start env-stop
+
+ELASTICSEARCH_CONTAINER := learn-elasticsearch
+ELASTICSEARCH_LEARN_USER := learn
+ELASTICSEARCH_LEARN_USER_PASSWORD := 123456
+ELASTICSEARCH_ELASTIC_USER_PASSWORD := 123456
+KIBANA_CONTAINER := learn-kibana
+
+jar-build:
+	mvn clean package
+
+tests-run:
+	mvn test
+
+env-start:
+	docker-compose up -d --remove-orphans
+
+env-stop:
+	docker-compose stop
+
+es-create-token:
+	@ docker exec $(ELASTICSEARCH_CONTAINER) ./bin/elasticsearch-create-enrollment-token -s kibana
+
+kibana-verification-code:
+	@ docker exec $(KIBANA_CONTAINER) ./bin/kibana-verification-code
+
+es-recreate-elastic-users-passwords:
+	@ docker exec $(ELASTICSEARCH_CONTAINER) ./bin/elasticsearch-setup-passwords auto
+
+es-init-learn-user:
+	@ docker exec $(ELASTICSEARCH_CONTAINER) ./bin/elasticsearch-users useradd $(ELASTICSEARCH_LEARN_USER) -p $(ELASTICSEARCH_LEARN_USER_PASSWORD) -r superuser
--- src/main/java/com/github/hetikk/learn/Application.java
+++ src/main/java/com/github/hetikk/learn/Application.java
@@ -1,9 +1,37 @@
 package com.github.hetikk.learn;
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.core.CreateRequest;
+import co.elastic.clients.elasticsearch.core.CreateResponse;
+import co.elastic.clients.elasticsearch.core.SearchResponse;
+import co.elastic.clients.elasticsearch.core.search.Hit;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import com.fasterxml.jackson.databind.JsonNode;
+import lombok.SneakyThrows;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+
 public class Application {
 
+    @SneakyThrows
     public static void main(String[] args) {
-        System.out.println("Learn project!");
+        RestClient restClient = RestClient.builder(new HttpHost("localhost", 9200)).build();
+
+        ElasticsearchTransport transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
+
+        ElasticsearchClient client = new ElasticsearchClient(transport);
+
+        CreateRequest<Object> createRequest = new CreateRequest.Builder<>()
+                .index("blog")
+                .document("")
+                .build();
+        CreateResponse createResponse = client.create(createRequest);
+
+        for (Hit<?> hit : search.hits().hits()) {
+            System.out.println(hit.source());
+        }
     }
 
 }
